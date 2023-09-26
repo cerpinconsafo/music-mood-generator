c0b60d540b3caabef5286f20de838dd3624b2e1f
p.skol <pskolsky74@gmail.com>
2023-09-25 20:41:29 -0400

    trial ai

--- main.js
+++ main.js
@@ -1,51 +1,133 @@
-class MoodPlayer {
-    constructor() {
-      this.moodTagline = document.getElementById("moodTagline");
-      this.embed = document.getElementById("embed");
-    }
-  
-    removePreviousMoodText() {
-      this.moodTagline.innerText = "";
-    }
-  
-    playVideo(videoUrl) {
-      this.removePreviousMoodText();
-      this.moodTagline.innerText = videoUrl.tagline;
-      this.embed.src = videoUrl.url;
-    }
-  }
-  
-  class MoodButton {
-    constructor(id, tagline, url) {
-      this.button = document.querySelector(id);
-      this.tagline = tagline;
-      this.url = url;
-    }
-  
-    addClickListener(player) {
-      this.button.addEventListener("click", () => {
-        player.playVideo({ tagline: this.tagline, url: this.url });
-      });
-    }
-  }
-  
-  // Create a MoodPlayer instance
-  const moodPlayer = new MoodPlayer();
-  
-  // Create MoodButton instances for different moods
-  const moodButtons = [
-    new MoodButton('#phonkBut', "Yaooooo, Check out this Phonky shiiiii...", "https://www.youtube-nocookie.com/embed/dv5yx1g5j6E?&autoplay=1"),
-    new MoodButton('#chillBut', "Straight chillin...MEGA chillin...", "https://www.youtube.com/embed/j4sJkuOPUP8&autoplay=1"),
-    new MoodButton('#zoneBut', "On a futuristic dystopian ride through nostalgic voids", "https://www.youtube-nocookie.com/embed/F9L4q-0Pi4E?&autoplay=1"),
-    new MoodButton('#gameBut', "Retro Gamin', but for your ears", "https://www.youtube-nocookie.com/embed/DJ_wEoW8Qcg?&autoplay=1"),
-    new MoodButton('#miamiBut', "That 'Cruising in a drop down the Miami strip' Vibe", "https://www.youtube-nocookie.com/embed/85bkCmaOh4o?&autoplay=1"),
-    new MoodButton('#heavyBut', "Head Bangin energy", "https://www.youtube-nocookie.com/embed/MA2YTm7qjnE?&autoplay=1"),
-    new MoodButton('#mindBut', "Expand Your M I N D", "https://www.youtube-nocookie.com/embed/zjJB_dMuEtY?&autoplay=1"),
-    new MoodButton('#fourBut', "Get Ready To Move Your Body, BBY!", "https://www.youtube-nocookie.com/embed/bk6Xst6euQk?&autoplay=1"),
-  ];
-  
-  // Add click listeners for MoodButtons
-  moodButtons.forEach((button) => {
-    button.addClickListener(moodPlayer);
-  });
-  
+/*
+
+UPDATING THIS TO RUN DIFFERENTLY, BUT COMMITTING THE FIRST ITERATION OF THE PROJECT FOLDER.
+
+//on page load, only show MOOD buttons/list
+//on click, will dynamically populate 'vidArea' in HTML with embedded YOUTUBE code
+    //depending on the button (mood) clicked, a different URL variable from the moodArray can be inserted into the embed code (I THINK)
+*/
+//
+//
+//
+
+//globals IKNOW I KNOW WILL REFACTOR
+
+const moodTagline = document.getElementById("moodTagline");
+const embed =  document.getElementById("embed");
+//
+//
+//
+
+//button variables
+
+const phonkBut = document.querySelector('#phonkBut')
+const chillBut = document.querySelector('#chillBut')
+const zoneBut = document.querySelector('#zoneBut')
+const gameBut = document.querySelector('#gameBut')
+const miamiBut = document.querySelector('#miamiBut')
+const heavyBut = document.querySelector('#heavyBut')
+const mindBut = document.querySelector('#mindBut')
+const fourBut = document.querySelector('#fourBut')
+//
+//
+//
+
+//event listeners
+
+phonkBut.addEventListener('click', phonkVids);
+chillBut.addEventListener('click', chillVids);
+zoneBut.addEventListener('click', zoneVids);
+gameBut.addEventListener('click', gameVids);
+miamiBut.addEventListener('click', miamiVids);
+heavyBut.addEventListener('click', heavyVids);
+mindBut.addEventListener('click', mindVids);
+fourBut.addEventListener('click', fourVids);
+//
+//
+//
+
+// functions
+
+function removePreviousMoodText() {
+    moodTagline.innerText= "";
+};
+
+function phonkVids() {   
+
+    console.log("pressed the Phonk button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Yaooooo, Check out this Phonky shiiiii...";   
+
+    let url = "https://www.youtube-nocookie.com/embed/dv5yx1g5j6E?&autoplay=1";
+    embed.src=url;   
+};
+
+ function chillVids() {
+    console.log("pressed the chill button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Straight chillin...MEGA chillin...";   
+    let url = "https://www.youtube.com/embed/j4sJkuOPUP8&autoplay=1";
+    embed.src=url;  
+};
+
+function zoneVids() {
+    console.log("pressed the zone button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "On a futuristic dystopian ride through nostlagic voids";   
+
+    let url = "https://www.youtube-nocookie.com/embed/F9L4q-0Pi4E?&autoplay=1";
+    embed.src=url;  
+};
+
+function gameVids() {
+    console.log("pressed the game button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Retro Gamin', but for your ears";   
+
+    let url = "https://www.youtube-nocookie.com/embed/DJ_wEoW8Qcg?&autoplay=1";
+    embed.src=url;
+};
+
+function miamiVids() {
+    console.log("pressed the game button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "That 'Cruising in a drop down the Miami strip' Vibe";   
+    
+    let url = "https://www.youtube-nocookie.com/embed/85bkCmaOh4o?&autoplay=1";
+    embed.src=url;
+};
+
+function heavyVids() {
+    console.log("pressed the heavyAF button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Head Bangin energy";   
+
+    let url = "https://www.youtube-nocookie.com/embed/MA2YTm7qjnE?&autoplay=1";
+    embed.src=url;
+};
+
+function mindVids() {
+    console.log("pressed the mind melt button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Expand Your M I N D";   
+
+    let url = "https://www.youtube-nocookie.com/embed/zjJB_dMuEtY?&autoplay=1";
+    embed.src=url;
+};
+
+function fourVids() {
+    console.log("pressed the four to floor button") 
+    removePreviousMoodText()
+    moodTagline.innerText += 
+    "Get Ready To Move Your Body, BBY!";   
+// solomun/kaytranada videos wont embed, need other options
+    let url = "https://www.youtube-nocookie.com/embed/bk6Xst6euQk?&autoplay=1";
+    embed.src=url;
+};
